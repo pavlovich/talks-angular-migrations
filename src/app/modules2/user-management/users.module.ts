a770cfe73813b44f14974c8a3dc86a90388eb51d
Peter Pavlovich <pavlovich@gmail.com>
2017-07-21 06:26:45 -0600

    Add pre-req dependencies (Common Module [@angular/common] and FormsModule [@angular/forms] as imports on the users module )

--- src/app/modules2/user-management/users.module.ts
+++ src/app/modules2/user-management/users.module.ts
@@ -1,8 +1,11 @@
 
 import { UserService } from './user.service';
 import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   providers: [UserService]
+  imports: [CommonModule, FormsModule],
 })
 export class UsersModule{}
