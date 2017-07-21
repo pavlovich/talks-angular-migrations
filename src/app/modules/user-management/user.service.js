4e4805aaa1517c7d87d1f1adc8a511f725e6780a
Peter Pavlovich <pavlovich@gmail.com>
2017-07-21 06:25:03 -0600

    Use the @angular/upgrade/static downgradeInjectable function to create an NG1 service by downgrading the NG2 service. Keep the same NG1 service name to avoid additional changes.

--- src/app/modules/user-management/user.service.js
+++ src/app/modules/user-management/user.service.js
@@ -1,59 +1,9 @@
 
 import { UsersModule } from './users.module';
+import { downgradeInjectable } from '@angular/upgrade/static';
+import { UserService as NG2UserService } from '../../modules2/user-management';
 
 UsersModule
-  .factory('UserService', [function(){
-    return new UserService();
-  }]);
+  .factory('UserService', downgradeInjectable(NG2UserService));
 
-export class User {
-  constructor(email, password) {
-    this.email = email;
-    this.password = password;
-  }
-}
-
-export class UserService {
-  constructor() {
-    this.currentUser = null;
-  }
-
-  createAccount(email, password){
-    this.currentUser = new User(email, password);
-    return this.currentUser;
-  };
-
-  login(email, password){
-    this.currentUser = new User(email, password);
-    return this.currentUser;
-  };
-
-  logout(){
-    this.currentUser = null;
-    return true;
-  };
-
-  changePassword(email, pasword, newPassword){
-    this.currentUser.password = newPassword;
-    return this.currentUser;
-  };
-
-  resetPassword(email){
-    alert('Reset password here!');
-    return true;
-  };
-
-  getCurrentUser(){
-    return this.currentUser;
-  };
-
-  getCurrentUserEmail(){
-    let user = this.getCurrentUser();
-    return user ? user.email : null;
-  };
-
-  isLoggedIn(){
-    return this.currentUser !== null;
-  }
-
-}
+export const UserService = NG2UserService;
