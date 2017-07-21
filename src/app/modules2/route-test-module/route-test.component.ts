363523ebc9ce5412b8b2f5030be18138d5a2f7fe
Peter Pavlovich <pavlovich@gmail.com>
2017-07-21 06:28:27 -0600

    Test the new component by adding the <user-controls></user-controls> tag to the template in the route-test-component.

--- src/app/modules2/route-test-module/route-test.component.ts
+++ src/app/modules2/route-test-module/route-test.component.ts
@@ -4,6 +4,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'route-test-cmp',
   template: `
+    <div class="navbar navbar-default" role="navigation">
+      <div class="navbar-collapse collapse">
+        <user-controls></user-controls>
+      </div>
+    </div>
     <div>Hello from the Route Test Component!</div>
   `,
   styles: ['div {text-align: center; padding: 10px; color: white;}']
