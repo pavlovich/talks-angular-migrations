0e4eaefdaa0e21a4913e90d7082686e916cfe663
Peter Pavlovich <pavlovich@gmail.com>
2017-07-21 06:25:44 -0600

    Prep UserControlsCmp to be componentized by converting controller function into a class.

--- src/app/modules/user-management/user-controls.js
+++ src/app/modules/user-management/user-controls.js
@@ -5,118 +5,111 @@ import $ from 'jquery';
 
 import { UsersModule } from './users.module';
 
+export class UserControlsCmp {
+  constructor($scope, UserService){
+    this.UserService = UserService;
+    this.resetForms();
+  }
+
+  resetForms(){
+    this.email = null;
+    this.password = null;
+    this.newPassword = null;
+    this.showDropDown = false;
+    this.showForgottenPasswordForm = false;
+    this.showCreateAccountForm = false;
+    this.showLoginPasswordForm = false;
+    this.showChangePasswordForm = false;
+    this.showLoggedInButtons = false;
+    if(this.isLoggedIn()){
+      this.showLoggedInButtons = true;
+    }else{
+      this.showLoginPasswordForm = true;
+    }
+  };
+
+  getCurrentUser(){
+    return this.UserService.getCurrentUser();
+  };
+
+  isLoggedIn(){
+    return this.getCurrentUser() !== null;
+  };
+
+  openDropDown(){
+    this.showDropDown = true;
+  };
+
+  showForgottenPassword(){
+    this.showLoginPasswordForm = false;
+    this.showForgottenPasswordForm = true;
+  };
+
+  showLoginPassword(){
+    this.showForgottenPasswordForm = false;
+    this.showCreateAccountForm = false;
+    this.showLoginPasswordForm = true;
+  };
+
+  showCreateAccount(){
+    this.showLoginPasswordForm = false;
+    this.showCreateAccountForm = true;
+  };
+
+  showChangePassword(){
+    this.showLoggedInButtons = false;
+    this.showChangePasswordForm = true;
+  };
+
+  changePassword(){
+    let res = this.UserService.changePassword(this.email, this.password, this.newPassword);
+    if(res){
+      this.resetForms();
+    }else{
+      alert('Error changing password!');
+    }
+  };
+
+  resetPassword(){
+    if(this.userService.resetPassword(this.email)){
+      this.resetForms();
+    }else{
+      alert('Error resetting password!');
+    }
+  };
+
+  createAccount(){
+    if(this.userService.createAccount(this.email, this.password)){
+      this.resetForms();
+    }else{
+      alert('Error creating account!');
+    }
+  };
+
+  login(){
+    let res = this.UserService.login(this.email, this.password);
+    if(res){
+      this.resetForms();
+    }else{
+      alert('Error logging in!');
+    }
+  };
+
+  logout(){
+    let res = this.UserService.logout();
+    if(res){
+      this.resetForms();
+    }
+  }
+
+}
+
 UsersModule
   .directive('userControls', [function () {
     return {
       restrict: 'E',
       controllerAs: 'userCtrl',
       template: require('./user-controls.html'),
-      controller: ['$scope', 'UserService',
-        function UserControlsCmpImpl ($scope, UserService){
-          let userCtrl = this;
-
-          userCtrl.UserService = UserService;
-
-          Object.assign(userCtrl, {
-
-            resetForms: function(){
-              userCtrl.email = null;
-              userCtrl.password = null;
-              userCtrl.newPassword = null;
-              userCtrl.showDropDown = false;
-              userCtrl.showForgottenPasswordForm = false;
-              userCtrl.showCreateAccountForm = false;
-              userCtrl.showLoginPasswordForm = false;
-              userCtrl.showChangePasswordForm = false;
-              userCtrl.showLoggedInButtons = false;
-              if(userCtrl.isLoggedIn()){
-                userCtrl.showLoggedInButtons = true;
-              }else{
-                userCtrl.showLoginPasswordForm = true;
-              }
-            },
-
-            getCurrentUser: function(){
-              return userCtrl.UserService.getCurrentUser();
-            },
-
-            isLoggedIn: function(){
-              return userCtrl.getCurrentUser() !== null;
-            },
-
-            openDropDown: function(){
-              userCtrl.showDropDown = true;
-            },
-
-            showForgottenPassword: function(){
-              userCtrl.showLoginPasswordForm = false;
-              userCtrl.showForgottenPasswordForm = true;
-            },
-
-            showLoginPassword: function(){
-              userCtrl.showForgottenPasswordForm = false;
-              userCtrl.showCreateAccountForm = false;
-              userCtrl.showLoginPasswordForm = true;
-            },
-
-            showCreateAccount: function(){
-              userCtrl.showLoginPasswordForm = false;
-              userCtrl.showCreateAccountForm = true;
-            },
-
-            showChangePassword: function(){
-              userCtrl.showLoggedInButtons = false;
-              userCtrl.showChangePasswordForm = true;
-            },
-
-            changePassword: function(){
-              let res = userCtrl.UserService.changePassword(userCtrl.email, userCtrl.password, userCtrl.newPassword);
-              if(res){
-                userCtrl.resetForms();
-              }else{
-                alert('Error changing password!');
-              }
-            },
-
-            resetPassword: function(){
-              if(userCtrl.userService.resetPassword(userCtrl.email)){
-                userCtrl.resetForms();
-              }else{
-                alert('Error resetting password!');
-              };
-            },
-
-            createAccount: function(){
-              if(userCtrl.userService.createAccount(userCtrl.email, userCtrl.password)){
-                userCtrl.resetForms();
-              }else{
-                alert('Error creating account!');
-              }
-            },
-
-            login: function(){
-              let res = userCtrl.UserService.login(userCtrl.email, userCtrl.password);
-              if(res){
-                userCtrl.resetForms();
-              }else{
-                alert('Error logging in!');
-              }
-            },
-
-            logout: function(){
-              let res = userCtrl.UserService.logout();
-              if(res){
-                userCtrl.resetForms();
-              }
-            }
-
-          });
-
-          userCtrl.resetForms();
-
-        }
-      ]
+      controller: ['$scope', 'UserService', UserControlsCmp]
     };
   }]);
-
-export const UserControlsCmp = 'UserControlsCmp';
