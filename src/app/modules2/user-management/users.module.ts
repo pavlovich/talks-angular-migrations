bdd6d3630f19e168fbf948712cb30f866239e22f
Peter Pavlovich <pavlovich@gmail.com>
2017-07-21 06:27:38 -0600

    Register the new UserControlsCmp in the module under declarations, exports and entryComponents.

--- src/app/modules2/user-management/users.module.ts
+++ src/app/modules2/user-management/users.module.ts
@@ -3,9 +3,13 @@ import { UserService } from './user.service';
 import {NgModule} from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UserControlsCmp } from './user-controls';
 
 @NgModule({
   providers: [UserService]
   imports: [CommonModule, FormsModule],
+  declarations: [UserControlsCmp],
+  exports: [UserControlsCmp],
+  entryComponents: [UserControlsCmp]
 })
 export class UsersModule{}
